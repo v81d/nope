7d220f851d228339ee07db8e4e4c666dc08da60c
v81d <v81d@proton.me>
2026-04-01 16:30:07 +0000

    feat: add CLI interface

---
+++ src/cli.rs
@@ -0,0 +1,31 @@
+use clap::{Args, Parser, Subcommand};
+
+///  A CLI tool for keeping track of commands you regret running.
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    List,
+    Add(AddArgs),
+    Remove(RemoveArgs),
+}
+
+#[derive(Args)]
+pub struct AddArgs {
+    /// Command to add to regrets list
+    pub command: String,
+
+    /// Brief description of regret
+    pub description: String,
+}
+
+#[derive(Args)]
+pub struct RemoveArgs {
+    /// Command to remove from regrets list
+    pub command: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod cli;
+mod config;
+
+use clap::Parser;
+use cli::{Cli, Commands};
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::List => println!("List something."),
+        Commands::Add(args) => println!("{} - {}", args.command, args.description),
+        _ => (),
+    }
 }
